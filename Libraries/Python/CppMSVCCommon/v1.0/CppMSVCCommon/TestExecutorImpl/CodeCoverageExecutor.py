59a5e4df0dbdbb69c24c4d204da50325af104a4c
David Brownell <db@DavidBrownell.com>
2019-04-25 20:31:55 -0700

    Using clang's code coverage extraction functionality when clang-cl is enabled

---
+++ Libraries/Python/CppMSVCCommon/v1.0/CppMSVCCommon/TestExecutorImpl/CodeCoverageExecutor.py
@@ -0,0 +1,167 @@
+# ----------------------------------------------------------------------
+# |
+# |  CodeCoverageExecutor.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2019-04-25 13:15:53
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2019
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+"""Contains the CodeCoverageExecutor object"""
+
+import csv
+from fnmatch import fnmatch
+import os
+import subprocess
+
+import CommonEnvironment
+from CommonEnvironment.CallOnExit import CallOnExit
+from CommonEnvironment import FileSystem
+from CommonEnvironment import Interface
+from CommonEnvironment import Process
+from CommonEnvironment.Shell.All import CurrentShell
+
+from CppCommon.CodeCoverageExecutor import CodeCoverageExecutor as CodeCoverageExecutorBase
+
+# ----------------------------------------------------------------------
+_script_fullpath                            = CommonEnvironment.ThisFullpath()
+_script_dir, _script_name                   = os.path.split(_script_fullpath)
+#  ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+@Interface.staticderived
+class CodeCoverageExecutor(CodeCoverageExecutorBase):
+    """Extracts code coverage using MSVC performance tools"""
+
+    # ----------------------------------------------------------------------
+    # |  Properties
+    DefaultFileName                         = Interface.DerivedProperty("code.coverage")
+    Units                                   = Interface.DerivedProperty("blocks")
+
+    # ----------------------------------------------------------------------
+    # |  Methods
+    @staticmethod
+    @Interface.override
+    def PreprocessBinary(binary_filename, output_stream):
+        FileSystem.RemoveFile("{}.orig".format(binary_filename))
+        return Process.Execute('vsinstr "{}" /COVERAGE'.format(binary_filename), output_stream)
+
+    # ----------------------------------------------------------------------
+    @classmethod
+    @Interface.override
+    def StartCoverage(cls, coverage_filename, output_stream):
+        # Shutdown any existing monitors
+        cls.StopCoverage(output_stream)
+
+        result, output = _ProcessExecuteWorkaround(
+            'VSPerfCmd.exe /WAITSTART /START:COVERAGE "/OUTPUT:{}"'.format(
+                coverage_filename,
+            ),
+        )
+        output_stream.write(output)
+
+        return result
+
+    # ----------------------------------------------------------------------
+    @staticmethod
+    @Interface.override
+    def StopCoverage(output_stream):
+        return Process.Execute("VSPerfCmd.exe /SHUTDOWN", output_stream)
+
+    # ----------------------------------------------------------------------
+    @staticmethod
+    @Interface.override
+    def ExtractCoverageInfo(coverage_filename, binary_filename, includes, excludes, output_stream):
+        if excludes:
+            excludes_func = lambda method_name: any(fnmatch(method_name, exclude) for exclude in excludes)
+        else:
+            excludes_func = lambda method_name: False
+
+        if includes:
+            includes_func = lambda method_name: any(fnmatch(method_name, include) for include in includes)
+        else:
+            includes_func = lambda method_name: True
+
+        # ----------------------------------------------------------------------
+        def ShouldInclude(method_name):
+            return not excludes_func(method_name) and includes_func(method_name)
+
+        # ----------------------------------------------------------------------
+
+        temp_filename = CurrentShell.CreateTempFilename()
+
+        command_line = '"{powershell}" -ExecutionPolicy Bypass -NoProfile -File "{filename}" "{coverage}" "{module}" > "{temp_filename}" 2>&1'.format(
+            powershell=r"{}\syswow64\WindowsPowerShell\v1.0\powershell.exe".format(
+                os.getenv("SystemRoot"),
+            ),
+            filename=os.path.join(_script_dir, "CoverageToCsv.ps1"),
+            coverage=coverage_filename,
+            module=os.path.basename(binary_filename),
+            temp_filename=temp_filename,
+        )
+
+        result = Process.Execute(command_line, output_stream)
+        if result != 0:
+            return result
+
+        with CallOnExit(lambda: FileSystem.RemoveFile(temp_filename)):
+            covered = 0
+            not_covered = 0
+
+            with open(temp_filename, "r") as input:
+                reader = csv.reader(input)
+
+                for row in reader:
+                    if not isinstance(row, (tuple, list)):
+                        raise Exception(row)
+
+                    method_name = row[1]
+                    if not ShouldInclude(method_name):
+                        continue
+
+                    covered += int(row[-2])
+                    not_covered += int(row[-1])
+
+            return covered, not_covered
+                
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+def _ProcessExecuteWorkaround(command_line):
+    # I haven't been able to figure out what is causing this, but it appears that VSPerfCmd.exe
+    # isn't sending an EOF before terminating during startup. This causes problems for subprocess-
+    # based means of forking processes and ends up hanging forever. To work around this, use
+    # subprocess.call, which doesn't use standard streams for input and output.
+    #
+    # Unfortunately, there isn't an easy way to capture the output from subprocess.call, so
+    # invoke the command with output redirected to a file, then extract the content from the
+    # file itself.
+    #
+    # This is all a great big hack.
+
+    temp_filename = CurrentShell.CreateTempFilename("._ProcessExecuteWorkaround.output")
+
+    command_line += ' > "{}" 2>&1'.format(temp_filename)
+
+    result = subprocess.call(
+        command_line,
+        shell=True,
+    )
+
+    assert os.path.isfile(temp_filename), temp_filename
+    with open(temp_filename) as f:
+        output = f.read()
+
+    # For some reason, this file is still in use when we attempt to delete it here.
+    # For now, don't delete it as it should be pretty small.
+    #
+    # FileSystem.RemoveFile(temp_filename)
+
+    return result, output
--- Libraries/Python/CppMSVCCommon/v1.0/CppMSVCCommon/TestExecutorImpl/__init__.py
+++ Libraries/Python/CppMSVCCommon/v1.0/CppMSVCCommon/TestExecutorImpl/__init__.py
@@ -15,24 +15,18 @@
 # ----------------------------------------------------------------------
 """Contains the TestExecutorImpl object"""
 
-import csv
 import datetime
-from fnmatch import fnmatch
 import os
-import subprocess
 import textwrap
 import threading
 import time
 
-from collections import OrderedDict, namedtuple
+from collections import OrderedDict
 
 import six
 
 import CommonEnvironment
-from CommonEnvironment.CallOnExit import CallOnExit
-from CommonEnvironment import FileSystem
 from CommonEnvironment import Interface
-from CommonEnvironment import Process
 from CommonEnvironment.Shell.All import CurrentShell
 from CommonEnvironment import StringHelpers
 from CommonEnvironment import TaskPool
@@ -40,6 +34,9 @@ from CommonEnvironment import TaskPool
 from CommonEnvironment.TestExecutorImpl import TestExecutorImpl as TestExecutorImplBase
 
 from CppCommon import CodeCoverageFilter
+
+from CppClangCommon.CodeCoverageExecutor import CodeCoverageExecutor as ClangCodeCoverageExecutor
+from CppMSVCCommon.TestExecutorImpl.CodeCoverageExecutor import CodeCoverageExecutor as MSVCCodeCoverageExecutor
 
 # ----------------------------------------------------------------------
 _script_fullpath                            = CommonEnvironment.ThisFullpath()
@@ -84,20 +81,20 @@ class TestExecutorImpl(TestExecutorImplBase):
         coverage_start_time = time.time()
         coverage_output = OrderedDict()
 
+        if os.getenv("CXX") == "clang-cl":
+            code_coverage_executor = ClangCodeCoverageExecutor()
+        else:
+            code_coverage_executor = MSVCCodeCoverageExecutor()
+
         # ----------------------------------------------------------------------
         def Impl():
             # Instrument the binaries
             on_status_update("Instrumenting Binaries")
 
-            command_line_template = 'vsinstr "{}" /COVERAGE'
-
             # ----------------------------------------------------------------------
             def Invoke(task_index, output_stream):
                 output_filename = context["output_filenames"][task_index]
-                if os.path.isfile("{}.orig".format(output_filename)):
-                    return 0
-
-                return Process.Execute(command_line_template.format(output_filename), output_stream)
+                return code_coverage_executor.PreprocessBinary(output_filename, output_stream)
 
             # ----------------------------------------------------------------------
 
@@ -115,48 +112,39 @@ class TestExecutorImpl(TestExecutorImplBase):
                 return
 
             # Start coverage
-            coverage_output_filename = os.path.join(context["output_dir"], "code.coverage")
+            coverage_output_filename = os.path.join(context["output_dir"], code_coverage_executor.DefaultFileName)
 
             on_status_update("Starting Coverage Monitor")
 
-            # Showdown any existing monitors
-            Process.Execute("VSPerfCmd.exe /SHUTDOWN")
-
-            # Start a new monitor
-            execute_result.CoverageResult, output = _ProcessExecuteWorkaround(
-                'VSPerfCmd.exe /WAITSTART /START:COVERAGE "/OUTPUT:{}"'.format(
-                    coverage_output_filename,
-                ),
-            )
-            coverage_output["Starting Coverage Monitor"] = output
+            sink = six.moves.StringIO()
+            execute_result.CoverageResult = code_coverage_executor.StartCoverage(coverage_output_filename, sink)
+            coverage_output["Starting Coverage Monitor"] = sink.getvalue()
 
             if execute_result.CoverageResult != 0:
                 return
 
-            # Stop code coverage monitoring once the test is complete
+            # Execute the test(s)
+            on_status_update("Testing")
 
-            # ----------------------------------------------------------------------
-            def StopCoverageMonitoring():
-                on_status_update("Stopping Coverage Monitor")
+            test_start_time = time.time()
 
-                execute_result.CoverageResult, output = Process.Execute("VSPerfCmd.exe /SHUTDOWN")
-                coverage_output["Stopping Coverage Monitor"] = output
+            sink = six.moves.StringIO()
+            execute_result.TestResult = code_coverage_executor.Execute(command_line, sink)
+            execute_result.TestOutput = sink.getvalue()
 
-            # ----------------------------------------------------------------------
+            execute_result.TestTime = datetime.timedelta(
+                seconds=(time.time() - test_start_time),
+            )
 
-            with CallOnExit(StopCoverageMonitoring):
-                # Execute the test(s)
-                on_status_update("Testing")
+            if execute_result.TestResult != 0:
+                return
 
-                test_start_time = time.time()
+            # Stop code coverage monitoring and extract the results
+            on_status_update("Stopping Coverage Monitor")
 
-                execute_result.TestResult, execute_result.TestOutput = Process.Execute(command_line)
-                execute_result.TestTime = datetime.timedelta(
-                    seconds=(time.time() - test_start_time),
-                )
-
-                if execute_result.TestResult != 0:
-                    return
+            sink = six.moves.StringIO()
+            execute_result.CoverageResult = code_coverage_executor.StopCoverage(sink)
+            coverage_output["Stopping Coverage Monitor"] = sink.getvalue()
 
             if execute_result.CoverageResult != 0:
                 return
@@ -174,80 +162,32 @@ class TestExecutorImpl(TestExecutorImplBase):
 
             on_status_update(status_template.format(nonlocals.remaining))
 
-            # The coverage file generated has content that can be extracted by .NET Assemblies.
-            # Invoke powershell (which can interface with .NET) to do this.
-            command_line_template = '"{powershell}" -ExecutionPolicy Bypass -NoProfile -File "{filename}" "{coverage}" "{{module}}" > "{{temp_filename}}" 2>&1'.format(
-                powershell=r"{}\syswow64\WindowsPowerShell\v1.0\powershell.exe".format(
-                    os.getenv("SystemRoot"),
-                ),
-                filename=os.path.join(_script_dir, "CoverageToCsv.ps1"),
-                coverage=coverage_output_filename,
-            )
-
-            # ----------------------------------------------------------------------
-            ModuleResult = namedtuple("ModuleResult", ["covered", "not_covered"])
-
             # ----------------------------------------------------------------------
             def Invoke(task_index, output_stream):
-                temp_filename = CurrentShell.CreateTempFilename()
-
                 output_filename = context["output_filenames"][task_index]
 
-                result = Process.Execute(
-                    command_line_template.format(
-                        module=os.path.basename(output_filename),
-                        temp_filename=temp_filename,
-                    ),
-                    output_stream,
+                # This is a filename that can be used to specify includes and excludes. Note that this
+                # does not correspond to an actual file, as we don't have that information available.
+                mock_filter_filename = os.path.join(
+                    context["input"],
+                    os.path.splitext(os.path.basename(output_filename))[0],
                 )
 
-                if result != 0:
-                    return result
+                includes, excludes = CodeCoverageFilter.GetFilters(mock_filter_filename)
 
-                with CallOnExit(lambda: FileSystem.RemoveFile(temp_filename)):
-                    # This is a filename that can be used to specify includes and excludes. Note that this
-                    # does not correspond to an actual file, as we don't have that information available.
-                    mock_filter_filename = os.path.join(
-                        context["input"],
-                        os.path.splitext(os.path.basename(output_filename))[0],
-                    )
+                covered, not_covered = code_coverage_executor.ExtractCoverageInfo(
+                    coverage_output_filename,
+                    output_filename,
+                    includes,
+                    excludes,
+                    output_stream,
+                )
+                
+                all_results[task_index] = (covered, not_covered)
 
-                    includes, excludes = CodeCoverageFilter.GetFilters(mock_filter_filename)
-
-                    # ----------------------------------------------------------------------
-                    def Include(method_name):
-                        if excludes and any(fnmatch(method_name, exclude) for exclude in excludes):
-                            return False
-
-                        if includes and not any(fnmatch(method_name, include) for include in includes):
-                            return False
-
-                        return True
-
-                    # ----------------------------------------------------------------------
-
-                    covered = 0
-                    not_covered = 0
-
-                    with open(temp_filename, "r") as input:
-                        reader = csv.reader(input)
-
-                        for row in reader:
-                            if not isinstance(row, (tuple, list)):
-                                raise Exception(row)
-
-                            method_name = row[1]
-                            if not Include(method_name):
-                                continue
-
-                            covered += int(row[-2])
-                            not_covered += int(row[-1])
-
-                    all_results[task_index] = ModuleResult(covered, not_covered)
-
-                    with nonlocals_lock:
-                        nonlocals.remaining -= 1
-                        on_status_update(status_template.format(nonlocals.remaining))
+                with nonlocals_lock:
+                    nonlocals.remaining -= 1
+                    on_status_update(status_template.format(nonlocals.remaining))
 
                 return 0
 
@@ -274,15 +214,15 @@ class TestExecutorImpl(TestExecutorImplBase):
 
             all_percentages = OrderedDict()
 
-            for output_name, results in zip(output_names, all_results):
-                total_covered += results.covered
-                total_not_covered += results.not_covered
+            for output_name, (covered, not_covered) in zip(output_names, all_results):
+                total_covered += covered
+                total_not_covered += not_covered
                 
-                result_blocks = results.covered + results.not_covered
+                result_blocks = covered + not_covered
                 
                 all_percentages[output_name] = (
-                    (float(results.covered) / result_blocks if result_blocks else 0.0) * 100.0,
-                    "{} of {} blocks covered".format(results.covered, result_blocks),
+                    (float(covered) / result_blocks if result_blocks else 0.0) * 100.0,
+                    "{} of {} {} covered".format(covered, result_blocks, code_coverage_executor.Units),
                 )
 
             total_blocks = total_covered + total_not_covered
@@ -324,39 +264,3 @@ class TestExecutorImpl(TestExecutorImplBase):
         execute_result.CoverageTime = str(execute_result.CoverageTime)
 
         return execute_result
-
-
-# ----------------------------------------------------------------------
-# ----------------------------------------------------------------------
-# ----------------------------------------------------------------------
-def _ProcessExecuteWorkaround(command_line):
-    # I haven't been able to figure out what is causing this, but it appears that VSPerfCmd.exe
-    # isn't sending an EOF before terminating during startup. This causes problems for subprocess-
-    # based means of forking processes and ends up hanging forever. To work around this, use
-    # subprocess.call, which doesn't use standard streams for input and output.
-    #
-    # Unfortunately, there isn't an easy way to capture the output from subprocess.call, so
-    # invoke the command with output redirected to a file, then extract the content from the
-    # file itself.
-    #
-    # This is all a great big hack.
-
-    temp_filename = CurrentShell.CreateTempFilename("._ProcessExecuteWorkaround.output")
-
-    command_line += ' > "{}" 2>&1'.format(temp_filename)
-
-    result = subprocess.call(
-        command_line,
-        shell=True,
-    )
-
-    assert os.path.isfile(temp_filename), temp_filename
-    with open(temp_filename) as f:
-        output = f.read()
-
-    # For some reason, this file is still in use when we attempt to delete it here.
-    # For now, don't delete it as it should be pretty small.
-    #
-    # FileSystem.RemoveFile(temp_filename)
-
-    return result, output
